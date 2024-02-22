6d371fd1f88ba9c647a816af2d3449047ffa74a7
Raghda <raghdafaris@gmail.com>
2024-02-22 11:44:51 +0900

    1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #calculate the size of the input matrix 
+  input_rows <- nrow(x)
+  input_cols <- ncol(x)
+  
+  # Create an initial matrix with the same dimensions as the input matrix
+  results <- matrix( nrow = input_rows , ncol = input_cols)
+  
+  #calculating the inverse of the matix x
+  inv_mat <- solve(x)
+  
+  # Cache the result in the matrix created ubove 
+  results <- inv_mat
+  
+  #return the updated matrix 
+  return(results)
 }
+results <- makeCacheMatrix()
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# this function checks the cach and x  
+cacheSolve <- function(results, x,...) {
+  
+  # Check if the result is already cached
+  if (!is.na(results)) {
+    print("Using cached result")
+    return(results)
+    
+  }
+  #if the inverse is not cached then calculate it using the input matrix 
+  data <- x
+  notcach <- solve(data, ...)
+  notcach
 }
+  
